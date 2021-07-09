4f5faa38971b29096193859a5864fbd34a691084
drdarian <dariantaban@gmail.com>
2021-07-09 16:44:48 -0700

    Tests finished

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -1,82 +1,80 @@
 pragma solidity ^0.4.17;
 
 contract CampaignFactory {
-  address[] public deployedCampaigns;
+    address[] public deployedCampaigns;
 
-  function createCampaign(uint minimum) public {
-    address newCampaign = new Campaign(minimum, msg.sender);
-    deployedCampaigns.push(newCampaign);
-  }
+    function createCampaign(uint minimum) public {
+        address newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);
+    }
 
-  function getDeployedCampaigns() public view returns (address[]) {
-    return deployedCampaigns;
-  }
+    function getDeployedCampaigns() public view returns (address[]) {
+        return deployedCampaigns;
+    }
 }
 
 contract Campaign {
-  struct Request {
-    string description;
-    uint value;
-    address recipient;
-    bool complete;
-    uint approvalCount;
-    mapping(address => bool) approvals;
-  }
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
+    }
 
-  Request[] public requests;
-  address public manager;
-  uint public minimumContribution;
-  mapping(address => bool) public approvers;
-  uint public approversCount;
+    Request[] public requests;
+    address public manager;
+    uint public minimumContribution;
+    mapping(address => bool) public approvers;
+    uint public approversCount;
 
-  modifier restricted() {
-    require(msg.sender == manager);
-    _;
-  }
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
 
-  function Campaign(uint minimum, address creator) public {
-    manager = creator;
-    minimumContribution = minimum;
-  }
+    function Campaign(uint minimum, address creator) public {
+        manager = creator;
+        minimumContribution = minimum;
+    }
 
-  function contribute() public payable {
-    require(msg.value > minimumContribution);
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
 
-    approvers[msg.sender] = true;
-    approversCount++;
-  }
+        approvers[msg.sender] = true;
+        approversCount++;
+    }
 
-  function createRequest(string description, uint value, address recipient) public restricted {
-    require(approvers[msg.sender]);
-    Request memory newRequest = Request({
-      description: description,
-      value: value,
-      recipient: recipient,
-      complete: false,
-      approvalCount: 0
-    });
+    function createRequest(string description, uint value, address recipient) public restricted {
+        Request memory newRequest = Request({
+           description: description,
+           value: value,
+           recipient: recipient,
+           complete: false,
+           approvalCount: 0
+        });
 
-    requests.push(newRequest);
-  }
+        requests.push(newRequest);
+    }
 
-  function approveRequest(uint index) public {
-    Request storage request = requests[index];
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
 
-    require(approvers[msg.sender]);
-    require(!requests[index].approvals[msg.sender]);
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
 
-    request.approvals[msg.sender] = true;
-    request.approvalCount++;
-  }
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
 
-  function finalizeRequest(uint index) public restricted {
-    Request storage request = requests[index];
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index];
 
-    require(request.approvalCount > (approversCount / 2));
-    require(!requests[index].complete);
+        require(request.approvalCount > (approversCount / 2));
+        require(!request.complete);
 
-    request.recipient.transfer(request.value);
-    requests[index].complete = true;
-  }
-
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
 }
