434c4840569435da66e07665cde9e2438b8ba383
drdarian <dariantaban@gmail.com>
2021-07-13 11:29:52 -0700

    Basic Header (navbar) component built

---
+++ .next/dist/components/Header.js
@@ -0,0 +1,47 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _semanticUiReact = require('semantic-ui-react');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = '/Users/dariantaban/Documents/scratchWork/projects/kickstart/components/Header.js';
+
+
+var Header = function Header(props) {
+  return _react2.default.createElement(_semanticUiReact.Menu, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    }
+  }, _react2.default.createElement(_semanticUiReact.Menu.Item, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    }
+  }, 'KickCoin'), _react2.default.createElement(_semanticUiReact.Menu.Menu, { position: 'right', __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    }
+  }, _react2.default.createElement(_semanticUiReact.Menu.Item, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    }
+  }, 'Campaigns'), _react2.default.createElement(_semanticUiReact.Menu.Item, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12
+    }
+  }, '+')));
+};
+
+exports.default = Header;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHMvSGVhZGVyLmpzIl0sIm5hbWVzIjpbIlJlYWN0IiwiTWVudSIsIkhlYWRlciIsInByb3BzIl0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQSxBQUFPOzs7O0FBQ1AsQUFBUzs7Ozs7OztBQUVULElBQU0sU0FBUyxTQUFULEFBQVMsT0FBQSxBQUFDLE9BQVUsQUFDeEI7eUJBQ0UsQUFBQzs7Z0JBQUQ7a0JBQUEsQUFDRTtBQURGO0FBQUEsR0FBQSxrQkFDRyxjQUFELHNCQUFBLEFBQU07O2dCQUFOO2tCQUFBO0FBQUE7QUFBQSxLQURGLEFBQ0UsQUFFQSw2QkFBQyxjQUFELHNCQUFBLEFBQU0sUUFBSyxVQUFYLEFBQW9CO2dCQUFwQjtrQkFBQSxBQUNFO0FBREY7cUJBQ0csY0FBRCxzQkFBQSxBQUFNOztnQkFBTjtrQkFBQTtBQUFBO0FBQUEsS0FERixBQUNFLEFBRUEsOEJBQUMsY0FBRCxzQkFBQSxBQUFNOztnQkFBTjtrQkFBQTtBQUFBO0FBQUEsS0FQTixBQUNFLEFBR0UsQUFHRSxBQUlQO0FBWkQsQUFjQTs7a0JBQUEsQUFBZSIsImZpbGUiOiJIZWFkZXIuanMiLCJzb3VyY2VSb290IjoiL1VzZXJzL2RhcmlhbnRhYmFuL0RvY3VtZW50cy9zY3JhdGNoV29yay9wcm9qZWN0cy9raWNrc3RhcnQifQ==
--- .next/dist/components/Layout.js
+++ .next/dist/components/Layout.js
@@ -8,6 +8,10 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
+var _Header = require('./Header');
+
+var _Header2 = _interopRequireDefault(_Header);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var _jsxFileName = '/Users/dariantaban/Documents/scratchWork/projects/kickstart/components/Layout.js';
@@ -17,20 +21,20 @@ var Layout = function Layout(props) {
   return _react2.default.createElement('div', {
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 5
-    }
-  }, _react2.default.createElement('h1', {
-    __source: {
-      fileName: _jsxFileName,
       lineNumber: 6
     }
-  }, 'HEADER'), props.children, _react2.default.createElement('h1', {
+  }, _react2.default.createElement(_Header2.default, {
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 8
+      lineNumber: 7
+    }
+  }), props.children, _react2.default.createElement('h1', {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
     }
   }, 'FOOTER'));
 };
 
 exports.default = Layout;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHMvTGF5b3V0LmpzIl0sIm5hbWVzIjpbIlJlYWN0IiwiTGF5b3V0IiwicHJvcHMiLCJjaGlsZHJlbiJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUEsQUFBTzs7Ozs7Ozs7O0FBRVAsSUFBTSxTQUFTLFNBQVQsQUFBUyxPQUFBLEFBQUMsT0FBVSxBQUN4Qjt5QkFDRSxjQUFBOztnQkFBQTtrQkFBQSxBQUNBO0FBREE7QUFBQSxHQUFBLGtCQUNBLGNBQUE7O2dCQUFBO2tCQUFBO0FBQUE7QUFBQSxLQURBLEFBQ0EsQUFDQyxpQkFGRCxBQUVPLEFBQ1AsMEJBQUEsY0FBQTs7Z0JBQUE7a0JBQUE7QUFBQTtBQUFBLEtBSkYsQUFDRSxBQUdBLEFBR0g7QUFSRCxBQVVBOztrQkFBQSxBQUFlIiwiZmlsZSI6IkxheW91dC5qcyIsInNvdXJjZVJvb3QiOiIvVXNlcnMvZGFyaWFudGFiYW4vRG9jdW1lbnRzL3NjcmF0Y2hXb3JrL3Byb2plY3RzL2tpY2tzdGFydCJ9
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHMvTGF5b3V0LmpzIl0sIm5hbWVzIjpbIlJlYWN0IiwiSGVhZGVyIiwiTGF5b3V0IiwicHJvcHMiLCJjaGlsZHJlbiJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUEsQUFBTzs7OztBQUNQLEFBQU8sQUFBWTs7Ozs7Ozs7O0FBRW5CLElBQU0sU0FBUyxTQUFULEFBQVMsT0FBQSxBQUFDLE9BQVUsQUFDeEI7eUJBQ0UsY0FBQTs7Z0JBQUE7a0JBQUEsQUFDQTtBQURBO0FBQUEsR0FBQSxrQkFDQSxBQUFDOztnQkFBRDtrQkFEQSxBQUNBLEFBQ0M7QUFERDtBQUFBLFlBREEsQUFFTyxBQUNQLDBCQUFBLGNBQUE7O2dCQUFBO2tCQUFBO0FBQUE7QUFBQSxLQUpGLEFBQ0UsQUFHQSxBQUdIO0FBUkQsQUFVQTs7a0JBQUEsQUFBZSIsImZpbGUiOiJMYXlvdXQuanMiLCJzb3VyY2VSb290IjoiL1VzZXJzL2RhcmlhbnRhYmFuL0RvY3VtZW50cy9zY3JhdGNoV29yay9wcm9qZWN0cy9raWNrc3RhcnQifQ==
