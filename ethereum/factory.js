69277d5c80ad9c357ecf61ec860aab8177bb963c
drdarian <dariantaban@gmail.com>
2021-07-13 20:39:14 -0700

    added getSummary and getRequestsCount - redepolyed contracts

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0xF1629C7315242C769FefB1Ba7A5e4Ad0aD402e2f'
+  '0x76a446279D8bF978b11bF21D8640895BEB1833c1'
 )
 
 export default instance;
